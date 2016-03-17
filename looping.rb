0bff975b7fe416bb485aceb30487271da00e92b3
mrrealer <dwayne.samuel@gmail.com>
2016-03-17 01:22:34 -0400

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,20 @@
 def looping
-	#your code here
+	counter = 0
+  loop do 
+    counter += 1
+  puts "This is the #{counter}th time you did this!"
+    if counter >=10
+      break
+    end
+  end
 end
 
-#call your method here
+looping
+
+def looping2
+  loop do 
+    puts "Wingardium Leviosa"
+  end
+end
+looping2
+
